88107c797637569dad2e8c630c21e4423295182a
Steve Juney <smrt.local@gmail.com>
2017-10-06 00:41:30 +0300

    initial

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import '../sass/common.scss';
+import App from "./components/App";
+
+render(<App />, document.getElementById('app'));
